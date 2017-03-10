3f2d68f48def3f68bacefd0e2aa47a40772ca965
Carlos Anguiano <carlos.anguiano@gmail.com>
2017-03-09 21:50:56 -0600

    Lista Dinamicas
    Lista Dinamicas

---
+++ app/src/main/java/com/devix/listadinamicas/MainActivity.java
@@ -0,0 +1,54 @@
+package com.devix.listadinamicas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button button;
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> arrayList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.edtUno);
+        button = (Button) findViewById(R.id.btnUno);
+        listView = (ListView) findViewById(R.id.listViewUno);
+
+        arrayList = new ArrayList<String>();
+
+        arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.custom_layout, arrayList);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Este es " + listView.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arrayList.add(editText.getText().toString());
+                arrayAdapter.notifyDataSetChanged();
+                editText.setText("");
+            }
+        });
+    }
+}
